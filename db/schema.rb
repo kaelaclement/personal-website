4d4d174613a23bb5e608452f8f4d11abba32a54e
kaelaclement <stephanie.kaela@gmail.com>
2020-06-24 17:06:57 +0100

    add styles

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200617133231) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
